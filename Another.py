0e33d5b3435b1fb0a97fed719dcd8fef221084e4
ge42woh <chenhao.huang@tum.de>
2024-04-19 15:13:16 +0200

    Uncle Barry

--- Another.py
+++ Another.py
@@ -1,1 +1,11 @@
 #Third Branch Would like to say hello
+
+import __hello__
+
+from __future__ import barry_as_FLUFL
+ 0 != 1
+# SyntaxError: with Barry as BDFL, use '<>' instead of '!='
+  
+ 0 <> 1
+
+ 1 <> 1
